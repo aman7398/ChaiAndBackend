15e3ad7693ad965ab0f7887b36184a6e9230a437
aman <amandonaditya298@gmail.com>
2024-09-13 16:10:53 +0530

    created user and video model.

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,9 @@
-class ApiResponse = {
-    constructor(statusCode, success, message, data) {
-        this.statusCode = statusCode;
-        this.success = statusCode <400;
-        this.message = message;
-        this.data = data;
-    }
+class ApiResponse {
+  constructor(statusCode, message, data) {
+    this.statusCode = statusCode;
+    this.success = statusCode < 400;
+    this.message = message;
+    this.data = data;
+  }
 }
+export { ApiResponse };
